bd539a3d175689e09cb49f95bf026df47a629085
Richard Silva <richardfsilva@protonmail.com>
2021-09-12 18:32:10 -0300

    Group by search

--- backend/src/main/java/com/devsuperior/dsvendas/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsvendas/controllers/SaleController.java
@@ -1,6 +1,10 @@
 package com.devsuperior.dsvendas.controllers;
 
+import java.util.List;
+
 import com.devsuperior.dsvendas.dto.SaleDTO;
+import com.devsuperior.dsvendas.dto.SaleSuccessDTO;
+import com.devsuperior.dsvendas.dto.SaleSumDTO;
 import com.devsuperior.dsvendas.service.SaleService;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,4 +27,16 @@ public class SaleController {
         Page<SaleDTO> list = service.findAll(pageable);
         return ResponseEntity.ok(list);
     } 
+
+    @GetMapping(value = "/amount-by-seller")
+    public ResponseEntity<List<SaleSumDTO>> amountGroupedBySeller() {
+        List<SaleSumDTO> list = service.amountGroupedBySeller();
+        return ResponseEntity.ok(list);
+    }
+
+    @GetMapping(value = "/success-by-seller")
+    public ResponseEntity<List<SaleSuccessDTO>> successGroupedBySeller() {
+        List<SaleSuccessDTO> list = service.successGroupedBySeller();
+        return ResponseEntity.ok(list);
+    }
 }
