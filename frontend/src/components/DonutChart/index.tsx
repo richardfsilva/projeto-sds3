8b8ef865a5d550e453f6782be856d2913166d76c
Richard Silva <richardfsilva@protonmail.com>
2021-09-12 20:46:19 -0300

    DonutChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,51 +1,50 @@
-import axios from 'axios';
-import Chart from 'react-apexcharts';
-import { SaleSum } from 'types/sale';
-import { BASE_URL } from 'utils/requests';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Chart from "react-apexcharts";
+import { SaleSum } from "types/sale";
+import { BASE_URL } from "utils/requests";
 
 type ChartData = {
-    labels: string[];
-    series: number[];
-}
+  labels: string[];
+  series: number[];
+};
 
-const DonutChart = () =>{
-    
-    // Forma Errada!
-    let chartData : ChartData = {
-        labels: [],
-        series: []
-    }
+const DonutChart = () => {
+  const [chartData, setChartData] = useState<ChartData>({
+    labels: [],
+    series: [],
+  });
 
-    // Forma Errada!
-    axios.get(`${BASE_URL}/sales/amount-by-seller`)
-    .then( res => {
-        const data = res.data as SaleSum[];
-        const myLabels = data.map(x => x.sellerName);
-        const mySeries = data.map(x => x.sum);
-        
-        chartData = { labels: myLabels, series: mySeries };
-    })
+  useEffect(() => {
+    axios.get(`${BASE_URL}/sales/amount-by-seller`).then((res) => {
+      const data = res.data as SaleSum[];
+      const myLabels = data.map((x) => x.sellerName);
+      const mySeries = data.map((x) => x.sum);
 
-    /*const mockData = {
+      setChartData({ labels: myLabels, series: mySeries });
+    });
+  }, []);
+
+
+  /*const mockData = {
         series: [477138, 499928, 444867, 220426, 473088],
         labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
     } */
-    
-    const options = {
-        legend: {
-            show: true
-        }
-    }
-    
-    return (
-       <Chart 
-            options={{...options, labels: chartData.labels}}
-            series={chartData.series}
-            type="donut"
-            height="240"
-       />
-    );
-  }
-  
-  export default DonutChart;
-  
+
+  const options = {
+    legend: {
+      show: true,
+    },
+  };
+
+  return (
+    <Chart
+      options={{ ...options, labels: chartData.labels }}
+      series={chartData.series}
+      type="donut"
+      height="240"
+    />
+  );
+};
+
+export default DonutChart;
