6e4a0e5fadf881ab4393ab0cf381aac9122ba85e
Richard Silva <richardfsilva@protonmail.com>
2021-09-12 21:09:51 -0300

    BarChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -18,18 +18,12 @@ const DonutChart = () => {
   useEffect(() => {
     axios.get(`${BASE_URL}/sales/amount-by-seller`).then((res) => {
       const data = res.data as SaleSum[];
-      const myLabels = data.map((x) => x.sellerName);
-      const mySeries = data.map((x) => x.sum);
+      const myLabels = data.map( x => x.sellerName);
+      const mySeries = data.map( x => x.sum);
 
       setChartData({ labels: myLabels, series: mySeries });
     });
   }, []);
-
-
-  /*const mockData = {
-        series: [477138, 499928, 444867, 220426, 473088],
-        labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    } */
 
   const options = {
     legend: {
